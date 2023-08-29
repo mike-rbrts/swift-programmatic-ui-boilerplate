f777640b998a5cb2918e0f9ba91bba6acc1f05ad
Michael Roberts <roberts.b.michael@gmail.com>
2023-08-29 16:50:42 -0500

    MR | Swift Programmatic UI Boilerplate

---
+++ Project/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Project
+//
+//  Created by Mike Roberts on 8/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
